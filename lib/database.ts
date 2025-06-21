1ec7b22b5e23137b6de043348272cb32e1a5a81b
Anythingliched <shravan.r.chaursiya@gmail.com>
2025-06-21 19:32:15 +0530

    lib folder update

--- lib/database.ts
+++ lib/database.ts
@@ -2,15 +2,24 @@ import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI!;
 
+
 if (!MONGODB_URI) {
   throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
 }
 
-let cached = global.mongoose;
+type MongooseCache = { conn: typeof mongoose | null; promise: Promise<typeof mongoose> | null };
 
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
+declare global {
+  // eslint-disable-next-line no-var
+  var mongoose: MongooseCache | undefined;
 }
+
+let cached: MongooseCache;
+
+if (!global.mongoose) {
+  global.mongoose = { conn: null, promise: null };
+}
+cached = global.mongoose;
 
 async function connectDB() {
   if (cached.conn) {
